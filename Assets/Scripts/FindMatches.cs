9dd52e5e889a938d282473d4ccac4cd536580ac8
HelloWoori <myahn0607@gmail.com>
2019-01-28 21:00:41 +0900

    Part 19 : Generating Bombs Part 2

--- Assets/Scripts/FindMatches.cs
+++ Assets/Scripts/FindMatches.cs
@@ -194,7 +194,25 @@ public class FindMatches : MonoBehaviour
             //Is the other piece matched?
             else if (null != board.currentDot.otherDot)
             {
-
+                Dot otherDot = board.currentDot.otherDot.GetComponent<Dot>();
+                //Is the other Dot matched?
+                if (otherDot.isMatched)
+                {
+                    //Make it unmatched
+                    otherDot.isMatched = false;
+                    //Decide what kind of bomb to make
+                    int typeOfBomb = Random.Range(0, 100);
+                    if (typeOfBomb < 50)
+                    {
+                        //Make a row bomb
+                        otherDot.MakeRowBomb();
+                    }
+                    else if (typeOfBomb >= 50)
+                    {
+                        //Make a column bomb
+                        otherDot.MakeColumnBomb();
+                    }
+                }
             }
         }
     }
